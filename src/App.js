ff4ce8473ce48c6bf2c896de57a92a54683f83f5
Tahmid11 <tahmid.uddin11@outlook.com>
2022-03-14 15:56:29 +0000

    I have made a change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
 function App() {
   const [Location,setLocation]=useState('')
   const [data,setData]=useState({})
-  const url2=`http://api.openweathermap.org/geo/1.0/direct?q=${Location},44 &limit=3&appid=d559bc18da1537d61bf16fb76b5e30d5`
+  
   const url=`http://api.openweathermap.org/data/2.5/weather?q=${Location}&units=metric&APPID=d559bc18da1537d61bf16fb76b5e30d5`
   const searchLocation=(EVENT) =>{
     if (EVENT.key==="Enter"){
