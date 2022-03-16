2e836569c43158a0b9db4805b46b0ff4b59f9326
Tahmid11 <tahmid.uddin11@outlook.com>
2022-03-16 21:28:00 +0000

    Changes to the App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FirstPage from "./Components/FirstPage";
 import SecondPage from "./Components/SecondPage";
 import ThirdPage from "./Components/ThirdPage";
 import { useState } from "react";
+import { useEffect } from "react";
 import axios from 'axios';
 
 import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
@@ -12,36 +13,40 @@ import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
 // Use back ticks for links (`) and then use $(variableName) to call the variable.
 
 function App() {
-  const [Location,setLocation]=useState('');
-  const [data,setData]=useState({});
-  const [weather,setWeather]=useState({});
+  // const [Location,setLocation]=useState('');
+  // const [weather,setWeather]=useState([]);
+
+
+
   
 
 
 
-  // const url2=`http://api.openweathermap.org/geo/1.0/direct?q=${Location},44 &limit=3&appid=d559bc18da1537d61bf16fb76b5e30d5`
-  // const url3=`http://api.openweathermap.org/data/2.5/weather?q=${Location}&units=metric&APPID=d559bc18da1537d61bf16fb76b5e30d5`
-  const url=`https://api.openweathermap.org/geo/1.0/direct?q=${Location},GB&limit=1&appid=d559bc18da1537d61bf16fb76b5e30d5`
-  // const specialUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=d559bc18da1537d61bf16fb76b5e30d5`
-  // const url2=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely&appid=d559bc18da1537d61bf16fb76b5e30d5`
-  const searchLocation=(EVENT) =>{
-    if (EVENT.key==="Enter"){
-      axios.get(url).then((Response)=>{
-        setData(Response.data)
-        // console.log(Response.data)
-        const lat=(Response.data[0].lat)
-        const lon=(Response.data[0].lon)
-        console.log(lat)
-        console.log(lon)
+  // const url=`https://api.openweathermap.org/geo/1.0/direct?q=${Location},GB&limit=1&appid=d559bc18da1537d61bf16fb76b5e30d5`
+ 
+  // const searchLocation=(EVENT) =>{
+
+  //   if (EVENT.key==="Enter"){
+
+  //     axios.get(url).then((Response)=>{ 
+        
+
+  //       setLocation('')
+  //       let lat=(Response.data[0].lat)
+  //       let lon=(Response.data[0].lon)
+        
+  //     axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely&appid=d559bc18da1537d61bf16fb76b5e30d5&units=metric`).then((Reply)=>{
+  //     setWeather(Reply.data)
 
 
-      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely&appid=d559bc18da1537d61bf16fb76b5e30d5&units=metric`).then((Reply)=>{
-        setWeather(Reply.weather)
-        console.log(Reply)
-        })
-    })
+  //     // WHEN YOU PRINT CONSOLE.LOG(WEATHER) IT DONT WORK.
+  //     })
 
-  }}
+  //       })
+  
+
+  //     }}
+
 
   return (
     <div className="App">
@@ -62,9 +67,20 @@ function App() {
           </Routes>
           
       </Router>
-      <div className="searchBar">
-        <input type="text" value={Location} className="searchBar" onChange={EVENT=> setLocation(EVENT.target.value)} placeholder="Type a Location" onKeyPress={searchLocation}></input>
-      </div>
+      {/* <input type="text" value={Location} className="searchBar" onChange={EVENT=> setLocation(EVENT.target.value)} placeholder="Type a Location" onKeyPress={searchLocation}   ></input>
+
+      
+    {(!typeof weather.current!="undefined" && weather.length!=0)? (
+    <div>
+      {console.log(weather)}
+      {weather.current.temp},
+
+      {weather.current.clouds},
+      {weather.current.wind_speed}
+    </div>
+
+    ):('')} */}
+
       
 
 
@@ -75,7 +91,10 @@ function App() {
 
 
     </div>
-  );
+  )
 }
 
+ 
 export default App;
+
+
