eb07ae97e0b1416e95ea8020f3bbc7fde96e160d
Tahmid11 <tahmid.uddin11@outlook.com>
2022-03-16 21:32:58 +0000

    Changes to the firstPage

--- src/Components/FirstPage.js
+++ src/Components/FirstPage.js
@@ -1,34 +1,90 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
 
-// function FirstPage(){
-//     let navigate=useNavigate();
-//     function Handle(){
-//         navigate("./Components/secondPage")
-//     }
-//     return(
-//         <div>
-//             <button onClick={Handle}>Go To Second Page</button>
-//             <p>This should only be seen in first page.</p>
-            
+import { useState } from "react";
 
-//         </div>
-    
-    
-//     )
-// }
+import axios from 'axios';
+
 
 function FirstPage(){
      const nav=useNavigate();
+     const [Location,setLocation]=useState('');
+    const [weather,setWeather]=useState([]);
 
-    return (<div>
+
+
+  
+
+
+
+  const url=`https://api.openweathermap.org/geo/1.0/direct?q=${Location},GB&limit=1&appid=d559bc18da1537d61bf16fb76b5e30d5`
+ 
+  const searchLocation=(EVENT) =>{
+
+    if (EVENT.key==="Enter"){
+
+      axios.get(url).then((Response)=>{ 
+        
+
+        setLocation('')
+        let lat=(Response.data[0].lat)
+        let lon=(Response.data[0].lon)
+        
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely&appid=d559bc18da1537d61bf16fb76b5e30d5&units=metric`).then((Reply)=>{
+      setWeather(Reply.data)
+
+
+
+      })
+
+        })
+  
+
+      }}
+
+    return (
+    <div>
         <h1>First Page</h1>
         <p>This is the First page</p>
-        
+
         <button onClick={() => nav("/ThirdPage")}>Go To Third Page</button>
         <button onClick={()=>nav(0)}>First Page</button>
         <button onClick={() => nav("/SecondPage")}>Go To Second Page</button>
+        <input type="text" value={Location} className="searchBar" onChange={EVENT=> setLocation(EVENT.target.value)} placeholder="Type a Location" onKeyPress={searchLocation}   ></input>
+    
+          
+        {(!typeof weather.current!="undefined" && weather.length!=0)? (
+        <div>
+          {console.log(weather)}
+          {weather.current.temp},
+    
+          {weather.current.clouds},
+          {weather.current.wind_speed}
         </div>
+    
+        ):('')}
+
+
+    </div>
+    
+    
+       
+          
+    
+          
+
+    
+          
+    
+    
+    
+    
+    
+    
+    
+    
+
+      
     )
 }
 export default FirstPage;
