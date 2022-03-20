511dca876ba141993f5f8b3a14cc1fd115afa409
afjal-h <nahidhussain687@gmail.com>
2022-03-20 22:56:10 +0000

    added a search bar at bottom

--- src/Components/FirstPage.js
+++ src/Components/FirstPage.js
@@ -8,8 +8,11 @@ import axios from 'axios';
 
 
 function FirstPage(){
+  let LocationInput ="";
      const nav=useNavigate();
      const [Location,setLocation]=useState('');
+     const [Location2,setLocation2]=useState('');
+
     const [weather,setWeather]=useState([]);
 
 
@@ -19,12 +22,14 @@ function FirstPage(){
 
 
   const url=`https://api.openweathermap.org/geo/1.0/direct?q=${Location},GB&limit=1&appid=d559bc18da1537d61bf16fb76b5e30d5`
- 
+
+
+
   const searchLocation=(EVENT) =>{
 
-    if (EVENT.key==="Enter"){
-
+    if (EVENT.key==="Enter"){      
       axios.get(url).then((Response)=>{ 
+        setLocation2(Location);
         
 
         setLocation('')
@@ -42,21 +47,30 @@ function FirstPage(){
   
 
       }}
+      
 
     return (
 
     <div>
-        <input type="text" value={Location} className="searchBar" onChange={EVENT=> setLocation(EVENT.target.value)} placeholder="Type a Location" onKeyPress={searchLocation}   ></input>
-
+      <h1>{Location2}</h1>
+        
         <button onClick={() => nav("/ThirdPage")}>Go To Third Page</button>
         <button onClick={() => nav("/SecondPage")}>Go To Second Page</button>
+
+
+        <input type="text" value={Location} className="searchBar" onChange={EVENT=> setLocation(EVENT.target.value)} placeholder="Type a Location" onKeyPress={searchLocation} ></input>
+
+
+        
     
           
         {(!typeof weather.current!="undefined" && weather.length!=0)? (
-        <div>
+          
+        
+        <div className="results">
           {console.log(weather)}
+          {weather.current.weather[0].description},
           {weather.current.temp},
-    
           {weather.current.clouds},
           {weather.current.wind_speed}
         </div>
@@ -65,23 +79,6 @@ function FirstPage(){
 
 
     </div>
-    
-    
-       
-          
-    
-          
-
-    
-          
-    
-    
-    
-    
-    
-    
-    
-    
 
       
     )
