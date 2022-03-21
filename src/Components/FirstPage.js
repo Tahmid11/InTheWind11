017d2b9de645e1bad47134e7738f5d1a11e79814
Tahmid11 <tahmid.uddin11@outlook.com>
2022-03-21 16:11:00 +0000

    The dub

--- src/Components/FirstPage.js
+++ src/Components/FirstPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import {useNavigate} from "react-router-dom";
 
@@ -14,6 +14,8 @@ function FirstPage(){
      const [LocationAPI,setLocation2]=useState('');
      const [LocationSAVE,setLocationSAVE]=useState('');
 
+
+
     const [weather,setWeather]=useState([]);
 
   const url=`https://api.openweathermap.org/geo/1.0/direct?q=${Location},GB&limit=1&appid=d559bc18da1537d61bf16fb76b5e30d5`
@@ -21,7 +23,19 @@ function FirstPage(){
   //saving data inputs 
 
 
+useEffect( () => {
+  if (localStorage.getItem('Weather') === null) {
+    // set the weather state
+    console.log("It is empty");
 
+  } 
+  else{
+    console.log("IT is not empty.")
+    // String->JSON Object
+    const weatherObject = JSON.parse(localStorage.getItem('Weather'));
+    setWeather(weatherObject);
+  }
+}, []);
 
 
   const searchLocation=(EVENT) =>{
@@ -31,20 +45,27 @@ function FirstPage(){
       
       axios.get(url).then((Response)=>{ 
         
-        axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${Location},GB&limit=1&appid=d559bc18da1537d61bf16fb76b5e30d5`).then((Reply)=>{
-          setLocation2(Reply.data);
-          setLocationSAVE(Reply.data);
+
+          setLocation2(Response.data);
+          setLocationSAVE(Response.data);
+        
+          
     
     
-    
-          })
+
 
         setLocation('')
         let lat=(Response.data[0].lat)
         let lon=(Response.data[0].lon)
+
         
       axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely&appid=d559bc18da1537d61bf16fb76b5e30d5&units=metric`).then((Reply)=>{
       setWeather(Reply.data)
+      // JSON Object ->String.
+      localStorage.setItem('Weather', JSON.stringify(Reply.data));
+      
+
+
 
 
 
@@ -55,16 +76,21 @@ function FirstPage(){
 
       }}
 
-    React.useEffect(() =>{
-      localStorage.setItem('location', JSON.stringify(LocationSAVE));
-    });
+    // React.useEffect(() =>{
+    //   {
+    //   localStorage.setItem('location', JSON.stringify(LocationSAVE));
+    // }});
 
-    React.useEffect(() =>{
-      const data = localStorage.getItem('location');
-      if (data){
-        console.log(data);
-      }
-    });
+    // React.useEffect(() =>{
+    //   const data = localStorage.getItem('location');
+    //   if (data){
+    //     console.log(data);
+    //     return data
+
+
+    //   }
+      
+    // }
 
 
       
@@ -82,22 +108,24 @@ function FirstPage(){
         <input type="text" value={Location} className="searchBar" onChange={EVENT=> setLocation(EVENT.target.value)} placeholder="Type a Location" onKeyPress={searchLocation} ></input>
 
 
-        
+        {weather.length !== 0 ? <div className="results">
+          {weather.current.weather[0].description},
+          {weather.current.temp},
+          {weather.current.clouds},
+          {weather.current.wind_speed}
+          {console.log(localStorage.getItem('location'))}
+
+        </div> : <div></div>}
     
-          
+{/*           
         {(!typeof weather.current!="undefined" && weather.length!=0)? (
                   document.getElementById("LocationHead").innerHTML = LocationAPI[0].name,
           
           
         
-        <div className="results">
-          {weather.current.weather[0].description},
-          {weather.current.temp},
-          {weather.current.clouds},
-          {weather.current.wind_speed}
-        </div>
+
     
-        ):('')}
+        ):('')} */}
 
 
     </div>
@@ -107,3 +135,8 @@ function FirstPage(){
 }
 
 export default FirstPage;
+
+
+
+// If the local storage is not blank; you set the weather state using the local storage values.
+// A component is loaded which function is called? 
