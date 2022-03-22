ffe3641782a604a9b31e9a2e5a84a6f86f13b790
Tahmid11 <tahmid.uddin11@outlook.com>
2022-03-22 21:19:25 +0000

    Changes to App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@
 import FirstPage from "./Components/FirstPage";
 import SecondPage from "./Components/SecondPage";
 import ThirdPage from "./Components/ThirdPage";
-import { useState } from "react";
-import { useEffect } from "react";
-import axios from 'axios';
+
+
 
 
 // Import Swiper React components
@@ -28,41 +27,10 @@ import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
 
 
 
+
 function App() {
   // const [Location,setLocation]=useState('');
-  const [temp,setTemp]=useState({});
-
-
-
-
   
-
-
-
-  // const url=`https://api.openweathermap.org/geo/1.0/direct?q=${Location},GB&limit=1&appid=d559bc18da1537d61bf16fb76b5e30d5`
- 
-  // const searchLocation=(EVENT) =>{
-
-  //   if (EVENT.key==="Enter"){
-
-  //     axios.get(url).then((Response)=>{ 
-        
-
-  //       setLocation('')
-  //       let lat=(Response.data[0].lat)
-  //       let lon=(Response.data[0].lon)
-        
-  //     axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely&appid=d559bc18da1537d61bf16fb76b5e30d5&units=metric`).then((Reply)=>{
-  //     setWeather(Reply.data)
-
-
-  //     // WHEN YOU PRINT CONSOLE.LOG(WEATHER) IT DONT WORK.
-  //     })
-
-  //       })
-  
-
-  //     }}
   var varTemp= localStorage.getItem('temp');
 
 
@@ -73,7 +41,7 @@ function App() {
 
     <div className={
       (typeof varTemp != "null") 
-      ? ((varTemp>10)
+      ? ((varTemp>15)
       ? 'background2' : 'background1') :'background3'}>
       <main>
       <div className="App">
@@ -86,7 +54,6 @@ function App() {
 
             <Route path="/" element={<FirstPage/>}></Route>
             <Route path="/FirstPage" element={<FirstPage/>}></Route>
-    
             <Route path="/SecondPage" element={<SecondPage/>}/>
             <Route path="/ThirdPage" element={<ThirdPage/>}/>
           
